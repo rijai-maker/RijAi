564b23e06d92abe17b68e7fb907b192e566b1c02
rijai-maker <rijandhakal98765@gmail.com>
2026-07-22 17:02:50 +0545

    Remove CORS middleware and update log message

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -11,5 +9,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("RijAI server started");
+  console.log("RijAI is running");
 });
