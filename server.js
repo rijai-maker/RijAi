a683809a6af926f28f13e2247a3d00c40ad36731
rijai-maker <rijandhakal98765@gmail.com>
2026-07-22 16:55:56 +0545

    Update server response message and add CORS support

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("RijAI server is running 🤖");
+  res.send("RijAI server is connected 🤖");
 });
 
 app.listen(3000, () => {
