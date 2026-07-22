ded10c9b2413bd49a6e25442f1b72323d0694a1e
rijai-maker <rijandhakal98765@gmail.com>
2026-07-22 15:34:05 +0545

    Set up Express server with a basic route

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("RijAI server is running!");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("RijAI server is running 🤖");
+});
+
+app.listen(3000, () => {
+  console.log("RijAI server started");
+});
